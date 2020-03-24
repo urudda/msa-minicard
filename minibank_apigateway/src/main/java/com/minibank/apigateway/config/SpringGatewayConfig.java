e823506a2489b9dcda109289ad86fe8edd956bce
EUI DEOK LEE <sharplee7@gmail.com>
2020-03-24 18:09:17 +0900

    lab6-solution-1.0 revision

--- minibank_apigateway/src/main/java/com/minibank/apigateway/config/SpringGatewayConfig.java
+++ minibank_apigateway/src/main/java/com/minibank/apigateway/config/SpringGatewayConfig.java
@@ -1,8 +1,5 @@
 package com.minibank.apigateway.config;
 
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
